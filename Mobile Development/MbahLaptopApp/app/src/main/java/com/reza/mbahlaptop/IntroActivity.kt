ddf3dee12ab31073730bb78124519ff003303fb5
rezaHarsono <reza220304@gmail.com>
2024-11-18 14:42:53 +0700

    Create intro activity and app icon

---
+++ Mobile Development/MbahLaptopApp/app/src/main/java/com/reza/mbahlaptop/IntroActivity.kt
@@ -0,0 +1,23 @@
+package com.reza.mbahlaptop
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.reza.mbahlaptop.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
