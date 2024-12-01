5c28f097e896da0c4c7cedbb650938d6ffc0c6ce
Buzank <buzankkun@gmail.com>
2024-12-02 05:10:34 +0700

    feat(history): add a placeholder to HistoryFragment

--- Mobile Development/MbahLaptopApp/app/src/main/java/com/reza/mbahlaptop/ui/main/history/HistoryFragment.kt
+++ Mobile Development/MbahLaptopApp/app/src/main/java/com/reza/mbahlaptop/ui/main/history/HistoryFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.reza.mbahlaptop.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
 
     private var _binding: FragmentHistoryBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,16 +18,9 @@ class HistoryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val historyViewModel =
-            ViewModelProvider(this)[HistoryViewModel::class.java]
-
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.tvHistory
-        historyViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
--- Mobile Development/MbahLaptopApp/app/src/main/java/com/reza/mbahlaptop/ui/main/history/HistoryViewModel.kt
+++ Mobile Development/MbahLaptopApp/app/src/main/java/com/reza/mbahlaptop/ui/main/history/HistoryViewModel.kt
@@ -1,13 +1,6 @@
 package com.reza.mbahlaptop.ui.main.history
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HistoryViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is History Fragment"
-    }
-    val text: LiveData<String> = _text
 }
